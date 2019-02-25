37aabd4db31dc784fb979da6d1de3d5f850f7689
juan.pedraza <juan.pedraza@unionsoluciones.com.co>
2019-02-25 17:58:13 -0500

    ajustes formularios

--- formularios/src/app/components/template/template.component.ts
+++ formularios/src/app/components/template/template.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+      .ng-invalid.ng-touched:not(form){
+        border: 1px solid #f00;
+      }
+  `]
 })
 export class TemplateComponent implements OnInit {
+
+  usuario: object = {
+    nombre: null,
+    apellido: null,
+    correo: null
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  guardar(forma: NgForm) {
+    console.log("forma posteada");
+    console.log(forma);
+    console.log(forma.value);
+    console.log(this.usuario);
   }
 
 }
